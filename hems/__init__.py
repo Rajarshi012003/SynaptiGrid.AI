1f56130b300434628162015cc1cda94a71ec3823
Rajarshi012003 <santu1901@kgpian.iitkgp.ac.in>
2025-05-31 15:53:32 +0530

    Update __init__.py

--- hems/__init__.py
+++ hems/__init__.py
@@ -1,1 +1,26 @@
+"""
+Home Energy Management System (HEMS)
 
+A sophisticated energy management system using Spiking Neural Networks and Reinforcement Learning
+"""
+
+__version__ = '1.0.0'
+
+#  core components 
+from .rl_environment import HEMSEnvironment, OptimizationLayer
+from .rl_agent import train_rl_agent, evaluate_rl_agent, create_real_time_controller
+from .snn_model import SNN_Model, train_snn_model, test_snn_model
+from .data_processor import HEMSDataProcessor
+
+# "from hems import ...*"
+__all__ = [
+    'HEMSEnvironment',
+    'OptimizationLayer',
+    'train_rl_agent',
+    'evaluate_rl_agent',
+    'create_real_time_controller',
+    'SNN_Model',
+    'train_snn_model',
+    'test_snn_model',
+    'HEMSDataProcessor',
+] 
